92723594d4bcd1497581241457f151fb8065d79d
lxonfire <149894699+lxonfire@users.noreply.github.com>
2023-12-18 13:06:19 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Telegram webhook - Mr Tools</title>
+  <link href="./style.css" rel="stylesheet">
+</head>
+<body>
+  <div class="login-box">
+    <h2>Set up your PHP webhook</h2>
+    <form id="telegramForm">
+      <div class="user-box">
+        <input class="input" id="botToken" type="text" name="botToken" required maxlength="100" minlength="10">
+        <label>Enter Your bot token</label>
+      </div>
+      <div class="user-box">
+        <input class="input" id="dob" type="text" name="dob" required>
+        <label>Enter url</label>
+      </div>
+      <a href="#" onclick="setWebhook()" id="send">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        Set
+      </a>
+    </form>
+  </div>
+
+  <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+  <script>
+    function setWebhook() {
+      var botToken = document.getElementById('botToken').value;
+      var dob = document.getElementById('dob').value;
+
+      var url = `https://api.telegram.org/bot${botToken}/setWebhook?url={$webhookUrl}&dob=${dob}`;
+
+      $.ajax({
+        url: url,
+        type: 'GET',
+        success: function(response) {
+          console.log(response);
+          alert('Webhook set successfully!');
+        },
+        error: function(xhr, status, error) {
+          console.error(error);
+          alert('Failed to set webhook. Please check your details and try again.');
+        }
+      });
+    }
+  </script>
+</body>
+</html>
